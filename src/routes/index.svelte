1419a5551e0e942095566d9b09cc0c80bea81f2e
EtienneFR <etiennedayre10@gmail.com>
2020-03-01 18:03:52 +0100

    fix: make flexbox responsive and reduction transition timer

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -55,33 +55,29 @@
         </span>
     </div>
 
-    <div
-        class="flex flex-wrap p-2 justify-start sm:justify-center md:justify-end
-        lg:justify-between xl:justify-around">
-        <a href="e4" class="px-3">
-            <div
-                class="block max-w-sm rounded overflow-hidden shadow-lg
-                transition duration-700 ease-in-out bg-gray-100
-                hover:bg-gray-200 transform hover:-translate-y-1 hover:scale-110
-                px-3">
-                <img class="w-full" {src} alt="background image" />
-                <div class="px-3 py-2">
-                    <div class="font-bold text-xl mb-2">Épreuve E4 : PPE1</div>
-                    <p class="text-gray-700 text-base">Description here !</p>
-                </div>
+    <div class="flex flex-wrap p-2 justify-around">
+        <a
+            href="e4"
+            class="block w-full sm:max-w-sm rounded overflow-hidden shadow-lg
+            transition duration-200 ease-in-out bg-gray-100 hover:bg-gray-200
+            transform hover:-translate-y-1 hover:scale-110 m-3 px-3">
+
+            <img class="w-full" {src} alt="background image" />
+            <div class="px-3 py-2">
+                <div class="font-bold text-xl mb-2">Épreuve E4 : PPE1</div>
+                <p class="text-gray-700 text-base">Description here !</p>
             </div>
         </a>
-        <a href="e4" class="px-3">
-            <div
-                class="block max-w-sm rounded overflow-hidden shadow-lg
-                transition duration-700 ease-in-out bg-gray-100
-                hover:bg-gray-200 transform hover:-translate-y-1 hover:scale-110
-                px-2">
-                <img class="w-full" {src} alt="background image" />
-                <div class="px-3 py-2">
-                    <div class="font-bold text-xl mb-2">Épreuve E4 : PPE2</div>
-                    <p class="text-gray-700 text-base">Description here !</p>
-                </div>
+        <a
+            href="e4"
+            class="block w-full sm:max-w-sm rounded overflow-hidden shadow-lg
+            transition duration-200 ease-in-out bg-gray-100 hover:bg-gray-200
+            transform hover:-translate-y-1 hover:scale-110 m-3 px-2">
+
+            <img class="w-full" {src} alt="background image" />
+            <div class="px-3 py-2">
+                <div class="font-bold text-xl mb-2">Épreuve E4 : PPE2</div>
+                <p class="text-gray-700 text-base">Description here !</p>
             </div>
         </a>
     </div>
