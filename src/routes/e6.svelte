0c6db0db1a18cb744d97fb92e04a5477b3c52523
EtienneFR <etiennedayre10@gmail.com>
2020-03-15 22:44:13 +0100

    feat: add content on e6 page with pdf files

---
+++ src/routes/e6.svelte
@@ -0,0 +1,55 @@
+<script>
+    let pp = 'pp/report.svg';
+</script>
+
+<svelte:head>
+    <title>Épreuve E6</title>
+</svelte:head>
+<div class="w-full bg-gray-100">
+    <div class="container m-auto">
+
+        <div class="lg:w-full text-center m-auto py-8">
+            <h1 class="text-blue-600 font-mono text-2xl">Mon épreuve E6</h1>
+
+        </div>
+
+        <div class="p-4">
+            <span class="block font-mono text-center px-1 py-1">
+                Vous retrouverez sur cette page le résumé de mes deux PPE dans
+                le cadre de l'épreuve E4 du BTS SIO.
+            </span>
+        </div>
+
+        <div class="flex flex-wrap p-6 justify-around">
+            <div
+                id="ppe1"
+                class="block w-4/5 sm:max-w-sm rounded overflow-hidden shadow-lg
+                transition duration-100 ease-in-out bg-gray-100
+                hover:bg-gray-200 transform hover:-translate-y-1 hover:scale-110
+                m-8">
+
+                <img class="w-full" src={pp} alt="background image" />
+                <div class="px-3 py-2">
+                    <div class="font-bold font-mono text-xl mb-2 text-blue-600">
+                        Épreuve E6
+                    </div>
+                    <p class="text-gray-700 text-justify font-mono pr-3">
+                        Parcours de professionnalisation
+                    </p>
+
+                    <ul class="list-disc pt-3 ml-2">
+                        <li>
+                            <a href="pp/tableau.pdf">Tableau de synthèse</a>
+                        </li>
+                        <li>
+                            <a href="pp/presentation.pdf">
+                                Présentation parcours
+                            </a>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- src/routes/veilles.svelte
+++ src/routes/veilles.svelte
@@ -7,53 +7,58 @@
     <title>Veilles</title>
 </svelte:head>
 <div class="w-full bg-gray-100">
-    <div class="container" />
-    <div class="lg:w-full text-center m-auto py-8">
-        <h1 class="text-blue-600 font-mono text-2xl">Mes veilles</h1>
-    </div>
-    <div class="flex flex-wrap p-6 my-32 justify-around">
-        <div class="w-auto">
-            <a
-                href="veilles/veille-techno.pdf"
-                class="block w-full sm:max-w-sm rounded overflow-hidden
-                shadow-lg transition duration-100 ease-in-out bg-gray-100
-                hover:bg-gray-200 transform hover:-translate-y-1 hover:scale-110
-                my-8 px-2">
+    <div class="container m-auto">
+        <div class="lg:w-full text-center m-auto py-8">
+            <h1 class="text-blue-600 font-mono text-2xl">Mes veilles</h1>
+        </div>
+        <div class="flex flex-wrap p-6 justify-around">
+            <div class="w-auto">
+                <a
+                    href="veilles/veille-techno.pdf"
+                    class="block w-full sm:max-w-sm rounded overflow-hidden
+                    shadow-lg transition duration-100 ease-in-out bg-gray-100
+                    hover:bg-gray-200 transform hover:-translate-y-1
+                    hover:scale-110 my-8 px-2">
 
-                <img
-                    class="w-full object-scale-down h-32"
-                    src={car}
-                    alt="car" />
-                <div class="px-3 py-2">
-                    <div class="font-bold font-mono text-xl mb-2 text-blue-600">
-                        Veille Technologique
+                    <img
+                        class="w-full object-scale-down h-32"
+                        src={car}
+                        alt="car" />
+                    <div class="px-3 py-2">
+                        <div
+                            class="font-bold font-mono text-xl mb-2
+                            text-blue-600">
+                            Veille Technologique
+                        </div>
+                        <p class="text-gray-700 text-base font-mono">
+                            Voitures connectées
+                        </p>
                     </div>
-                    <p class="text-gray-700 text-base font-mono">
-                        Voitures connectées
-                    </p>
-                </div>
-            </a>
+                </a>
 
-            <a
-                href="veilles/veille-juridique.pdf"
-                class="block w-full sm:max-w-sm rounded overflow-hidden
-                shadow-lg transition duration-100 ease-in-out bg-gray-100
-                hover:bg-gray-200 transform hover:-translate-y-1 hover:scale-110
-                my-8 px-2">
+                <a
+                    href="veilles/veille-juridique.pdf"
+                    class="block w-full sm:max-w-sm rounded overflow-hidden
+                    shadow-lg transition duration-100 ease-in-out bg-gray-100
+                    hover:bg-gray-200 transform hover:-translate-y-1
+                    hover:scale-110 my-8 px-2">
 
-                <img
-                    class="w-full object-scale-down h-32"
-                    src={law}
-                    alt="car" />
-                <div class="px-3 py-2">
-                    <div class="font-bold font-mono text-xl mb-2 text-blue-600">
-                        Veille Juridique
+                    <img
+                        class="w-full object-scale-down h-32"
+                        src={law}
+                        alt="car" />
+                    <div class="px-3 py-2">
+                        <div
+                            class="font-bold font-mono text-xl mb-2
+                            text-blue-600">
+                            Veille Juridique
+                        </div>
+                        <p class="text-gray-700 text-base font-mono">
+                            Contrats et productions de services informatiques
+                        </p>
                     </div>
-                    <p class="text-gray-700 text-base font-mono">
-                        Contrats et productions de services informatiques
-                    </p>
-                </div>
-            </a>
+                </a>
+            </div>
         </div>
     </div>
 </div>
