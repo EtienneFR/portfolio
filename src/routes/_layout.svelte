97d54704046e63f81f61cb35fb8527cc7d3c3fbd
Etienne Dayre <etiennedayre10@gmail.com>
2021-01-06 01:10:10 +0100

    fix: dark mode contact page

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -7,7 +7,7 @@
 
 <Nav {segment} />
 
-<main class="relative flex flex-col flex-grow bg-gray-100 dark:bg-gray-700">
+<main class="relative flex flex-col flex-grow bg-gray-100">
     <slot />
 </main>
 
