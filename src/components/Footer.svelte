b6973cd16953b51efbf2990775c55ab2d4206419
Etienne Dayre <etiennedayre10@gmail.com>
2021-01-12 23:00:43 +0100

    feat: add location and country

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,3 +1,4 @@
 <footer class="inset-x-0 bottom-0 pb-3 bg-gray-100 dark:bg-gray-900">
     <div class="text-center dark:text-white">Etienne Dayre 2021.</div>
+    <div class="text-center dark:text-white">Paris, France.</div>
 </footer>
