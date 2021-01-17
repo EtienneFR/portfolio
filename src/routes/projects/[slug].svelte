76fdd4238aa8fb2cc5bfa2ccc8ebb1e96266e189
Etienne Dayre <etiennedayre10@gmail.com>
2021-01-17 17:55:39 +0100

    feat: use netlify cms to display projects cards

---
+++ src/routes/projects/[slug].svelte
@@ -0,0 +1,47 @@
+<script context="module">
+    export async function preload({ params, query }) {
+        // the `slug` parameter is available because
+        // this file is called [slug].svelte
+        const res = await this.fetch(`projects/${params.slug}.json`);
+        const data = await res.json();
+        if (res.status === 200) {
+            return { post: data };
+        } else {
+            this.error(res.status, data.message);
+        }
+    }
+</script>
+
+<script>
+    import Page from '../../components/Page.svelte';
+    import CardNotClickable from '../../components/CardNotClickable.svelte';
+    export let post;
+</script>
+
+<svelte:head>
+    <title>Etienne Dayre | {post.title}</title>
+</svelte:head>
+
+<Page>
+    <div class="container m-auto">
+
+        <div class="py-8 m-auto text-center lg:w-full">
+            <h1 class="text-2xl text-blue-600">Projets</h1>
+        </div>
+
+        <span class="block px-1 py-1 text-center">
+            Les deux projets dans le cadre du BTS SIO.
+        </span>
+
+        <div class="flex flex-col items-center p-6">
+
+            <CardNotClickable
+                id={post.id}
+                src={post.src}
+                alt={post.alt}
+                title={post.title}
+                content={post.content} />
+
+        </div>
+    </div>
+</Page>
---
+++ src/routes/projects/index.svelte
@@ -0,0 +1,45 @@
+<script context="module">
+    export function preload({ params, query }) {
+        return this.fetch(`projects.json`)
+            .then(r => r.json())
+            .then(posts => {
+                return { posts };
+            });
+    }
+</script>
+
+<script>
+    import Page from '../../components/Page.svelte';
+    import CardClickable from '../../components/CardClickable.svelte';
+
+    export let posts;
+</script>
+
+<svelte:head>
+    <title>Etienne Dayre | Projets</title>
+</svelte:head>
+
+<Page>
+    <div class="container m-auto">
+
+        <div class="py-8 m-auto text-center lg:w-full">
+            <h1 class="text-2xl text-blue-600">Projets</h1>
+        </div>
+
+        <span class="block px-1 py-1 text-center">
+            Les deux projets dans le cadre du BTS SIO.
+        </span>
+
+        <div class="flex flex-col items-center p-6">
+            {#each posts as post}
+                <CardClickable
+                    id={post.id}
+                    href="projects/{post.slug}"
+                    src={post.src}
+                    alt={post.alt}
+                    title={post.title}
+                    content={post.content} />
+            {/each}
+        </div>
+    </div>
+</Page>
