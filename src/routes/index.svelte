3b54960141667aae64586f058d16cf35fb716c51
Etienne Dayre <etiennedayre10@gmail.com>
2021-03-11 21:47:49 +0100

    fix: netlify cms config

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,18 @@
 <script>
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        if (window.netlifyIdentity) {
+            window.netlifyIdentity.on('init', user => {
+                if (!user) {
+                    window.netlifyIdentity.on('login', () => {
+                        document.location.href = '/admin/';
+                    });
+                }
+            });
+        }
+    });
+
     import Page from '../components/Page.svelte';
     import CardClickable from '../components/CardClickable.svelte';
 
