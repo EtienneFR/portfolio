7f160a6d32d3f7099170ffd836f0df129b822cde
Etienne Dayre <etiennedayre10@gmail.com>
2021-03-27 21:32:22 +0100

    feat: add arrow svg on link

--- src/routes/projects/[slug].svelte
+++ src/routes/projects/[slug].svelte
@@ -26,7 +26,23 @@
     <div class="container m-auto">
         <div
             class="hidden px-2 py-1 m-2 transition-colors duration-100 border-b-2 border-transparent border-blue-600 md:inline-block hover:border-blue-400">
-            <a sapper:prefetch href="projects">Retour aux projects</a>
+            <a sapper:prefetch href="projects">
+                <div class="flex items-center">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        class="object-scale-down mr-1 h-7 w-9">
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M7 16l-4-4m0 0l4-4m-4 4h18" />
+                    </svg>
+                    Projets
+                </div>
+            </a>
         </div>
         <div class="py-8 m-auto text-center lg:w-full">
             <h1 class="text-2xl text-blue-600">Projets</h1>
--- src/routes/watchs/[slug].svelte
+++ src/routes/watchs/[slug].svelte
@@ -27,7 +27,23 @@
 
         <div
             class="hidden px-2 py-1 m-2 transition-colors duration-100 border-b-2 border-transparent border-blue-600 md:inline-block hover:border-blue-400">
-            <a sapper:prefetch href="watchs">Retour aux veilles</a>
+            <a sapper:prefetch href="watchs">
+                <div class="flex items-center">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        class="object-scale-down mr-1 h-7 w-9">
+                        <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M7 16l-4-4m0 0l4-4m-4 4h18" />
+                    </svg>
+                    Veilles
+                </div>
+            </a>
         </div>
         <div class="py-8 m-auto text-center lg:w-full">
             <h1 class="text-2xl text-blue-600">Veilles</h1>
