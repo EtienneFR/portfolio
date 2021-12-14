035678882c6aaad57e6a39cfbed75591ded7bf42
Etienne Dayre <etiennedayre10@gmail.com>
2021-12-14 18:15:28 +0100

    fix: text

--- src/routes/projects/[slug].svelte
+++ src/routes/projects/[slug].svelte
@@ -48,7 +48,7 @@
                             stroke-width="2"
                             d="M7 16l-4-4m0 0l4-4m-4 4h18" />
                     </svg>
-                    Projets
+                    Projects
                 </div>
             </a>
         </div>
