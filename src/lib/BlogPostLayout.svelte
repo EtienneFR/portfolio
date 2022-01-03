844aadf68a6028e4737e12fcc54671a3526cf024
Etienne Dayre <etiennedayre10@gmail.com>
2022-01-03 10:14:32 +0100

    feat: improving blockquote of the article

--- src/lib/BlogPostLayout.svelte
+++ src/lib/BlogPostLayout.svelte
@@ -18,23 +18,31 @@
 
 <Page>
     <Animate>
-        <div slot="animation-content">
+        <article slot="animation-content">
             <div class="max-w-prose mx-auto md:w-2/3 px-3">
                 <h2 class="flex justify-center pt-5 text-2xl text-blue-900 dark:text-blue-300 font-semibold">
                     {title}
                 </h2>
-                <blockquote class="mt-3 pl-4 md:w-2/3 text-gray-800 dark:text-blue-100 italic text-base bg-indigo-100 dark:bg-indigo-900 border-l-4 border-purple-800 dark:border-purple-200">
-                    <span>Published:</span>
-                    <time date={formattedDate}>
-                        {formattedDate}
-                    </time>
+                <blockquote class="mt-3 pl-4 text-gray-800 dark:text-blue-100 text-base bg-indigo-100 dark:bg-indigo-900 border-l-4 border-purple-800 dark:border-purple-200">
+                    <span>
+                        Written by: 
+                        <a rel="author" href="/contact" class="italic hover:underline md:mr-2">Etienne Dayre</a>
+                    </span>
+                    <span class=" hidden sm:inline">|</span>
+                    <span class="block sm:inline md:ml-2">
+                        Published:
+                        <time date={formattedDate}>
+                            {formattedDate}
+                        </time>
+                    </span>
                 </blockquote>
+
                 <p class="text-gray-800 dark:text-blue-100 mt-4 font-semibold ">
                     {description}
                 </p>
                 <!-- wip: slot with the body of the article-->
                 <!-- <slot />-->
             </div>
-        </div>
+        </article>
     </Animate>
 </Page>
