03cafd9af7980b250dba0442f462c9fd53cc1aed
Etienne Dayre <etiennedayre10@gmail.com>
2022-01-05 13:18:50 +0100

    chore: automatically returns the date in the footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,4 +1,9 @@
+<script>
+    let date;
+
+    $: date = new Date().getFullYear();
+</script>
 <footer class="inset-x-0 bottom-0">
-    <div class="text-center">Etienne Dayre 2021.</div>
+    <div class="text-center">Etienne Dayre {date}</div>
     <div class="text-center">Paris, France.</div>
 </footer>
