74c39982e07cafbe8583590d005abf830e5c21d2
Etienne Dayre <etiennedayre10@gmail.com>
2022-01-21 11:51:31 +0100

    style: changed color of closeButtonPopup

--- src/lib/Nav/Popup/ClosePopupButton.svelte
+++ src/lib/Nav/Popup/ClosePopupButton.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <button
-	class="block absolute top-4 right-4 z-50 hover:text-gray-600 dark:hover:text-gray-400"
+	class="block absolute top-4 right-4 z-50 hover:text-gray-600 dark:hover:text-gray-400 dark:text-white"
 	on:click={onClose}
 >
 	<svelte:component this={X} class="h-5 w-5" />
